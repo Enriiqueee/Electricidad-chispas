5c0ba5a26b9c985764aac5b0a300cab11c3f33df
enriiquee <94898081+Enriiqueee@users.noreply.github.com>
2022-11-24 18:45:15 +0100

    24/11 agregada funcion de imprimir

--- src/InvoicePrinter.java
+++ src/InvoicePrinter.java
@@ -1,5 +1,8 @@
 public class InvoicePrinter {
     public void printFactura(Factura factura){
-        System.out.println("Animals:" + animals.getName() + " Edad:" + animals.getAge());
+        System.out.println("Factura: " + factura.getCodigofactura() + " | " + factura.getVenta() + "|" + factura.getFfactura());
+        System.out.println("Cliente: " +factura.getCliente().getId()+ " | " +factura.getCliente().getNombre()+ " | " +factura.getCliente().getPoblacion()+ " | " + factura.getCliente().getProvincia()+ " | " +factura.getCliente().getEmail());
+        System.out.println("Linea Venta (producto/servicio): " +factura.getVenta().getCodigoproducto()+ " | " +factura.getVenta().getNombre()+ " | " +factura.getVenta().getPrecio()+ " | " +factura.getVenta().getIVA()+ " | " +factura.getTota());
+        System.out.println("Total: " +factura.getTota());
     }
 }
